14c103d73001651cfc0aa06eb7a857b726cc5c39
kat <34391969+kat0154@users.noreply.github.com>
2019-09-28 15:31:50 -0400

    edit ndkwqon13i2901iA

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = process.env.TOKEN;
-
-exports.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-
-exports.DBL_KEY - process.env.DBL_KEY
+module.exports = {
+  TOKEN: process.env.TOKEN,
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  DBL_KEY: process.env.DBL_KEY
+}
