51c7e18a37a04dc360268a53ca4bc7ea08ce2cc6
kat <34391969+kat0154@users.noreply.github.com>
2019-09-28 16:04:54 -0400

    edit

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const music = require("../musicClient.js");
 const fs = require('fs');
 const moment = require("moment");
 const parse = require("parse-duration");
