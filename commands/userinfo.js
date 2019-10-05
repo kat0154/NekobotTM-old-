8a7608a94608f26e7555e5b1cede1c4d200d8cd7
kat <34391969+kat0154@users.noreply.github.com>
2019-10-04 22:10:09 -0400

    Y e e t

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -14,8 +14,8 @@ module.exports = {
 	},
 	run: async (client,message) => {
 		    let name = message.content.replace( /  +/g, ' ').split(" ").slice(1).join(" ");
-			var mem = message.mentions.members.first()||message.guild.members.get(name)||message.guild.members.find(m => m.displayName.toLowerCase().includes(name.toLowerCase()));
-			if (mem) {
+		    let mem = message.mentions.members.first()||message.guild.members.get(name)||message.guild.members.find(m => m.displayName.toLowerCase().includes(name.toLowerCase()))||message.member;
+			
 				var embed = new Discord.RichEmbed()
 				.setColor(`${mem.displayHexColor == '#000000' ? '#00ffff' : mem.displayHexColor}`)
 				.setTimestamp()
@@ -31,23 +31,7 @@ module.exports = {
 				.addField(`**Roles:**`, `${mem.roles.map(r => r).slice(1,16).join(' | ')}`)
 				.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
 				message.channel.send(embed);
-			}
-			if (!mem) {
-				var embed = new Discord.RichEmbed()
-				.setColor(`${message.member.displayHexColor == '#000000' ? '#00ffff' : message.member.displayHexColor}`)
-				.setTimestamp()
-				.setThumbnail(`${message.author.avatarURL}`)
-				.setAuthor(`Info on: ${message.author.tag}`, message.author.displayAvatarURL)
-				.addField(`**ID:**`, `${message.author.id}`)
-				.addField(`**Status:**`, `${message.member.presence.status}`)
-				.addField(`**Created:**`, `${moment(message.author.createdAt).format('MMMM Do YYYY, h:mm a')}`)
-				.addField(`**Joined:**`, `${moment(message.member.joinedAt).format('MMMM Do YYYY, h:mm a')}`)
-				.addField(`**Highest role?:**`, `${message.member.highestRole.name}`)
-				.addField(`**Hoist role:**`, `${message.member.hoistRole.name}`)
-				.addField(`**Display Color:**`, `${message.member.displayHexColor}`)
-				.addField(`**Roles:**`, `${message.member.roles.map(r => r).slice(1,16).join(' | ')}`)
-				.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
-				message.channel.send(embed);
+			
 			}
 	}
 }
