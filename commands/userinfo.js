a06bfbbb63c5b51245f7cc5c35b900a4ed600a95
kat <34391969+kat0154@users.noreply.github.com>
2019-10-04 20:45:29 -0400

    Y e e t

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,65 +1,53 @@
 const Discord = require("discord.js");
+const moment = require('moment');
 
-module.exports.run = async (client,message,args) => {
-const parseTime = function(milliseconds) {
-    var seconds = Math.floor(milliseconds/1000); milliseconds %= 1000;
-    var minutes = Math.floor(seconds/60); seconds %= 60;
-    var hours = Math.floor(minutes/60); minutes %= 60;
-    var days = Math.floor(hours/24); hours %= 24;
-    var years = Math.floor(days/365); days %= 365;
-    var written = false;
-    return(years?(written=true,years > 1 ? `${years} years` : `${years} year`):"")+(written?", ":"")
-      +(days?(written=true,days > 1 ? `${days} days` : `${days} day`):"")+(written?", ":"")
-      +(hours?(written=true,hours > 1 ? `${hours} hours` : `${hours} hour`):"")+(written?", ":"")
-      +(minutes?(written=true,minutes > 1 ? `${minutes} minutes` : `${minutes} minute`):"")+(written?", ":"")
-      +(seconds?(written=true,seconds > 1 ? `${seconds} seconds` : `${seconds} second`):"")+(written?" ":"");
-};
-var use = message.mentions.users.first();
-	var mem = message.mentions.members.first();
-	if (mem) {
-	var embed = new Discord.RichEmbed()
-	.setColor(`${mem.displayHexColor}`)
-	.setTimestamp()
-	.setThumbnail(`${use.displayAvatarURL}`)
-	.setTitle(`Userinfo for: ${mem.displayName}`)
-	.addField(`**Username & tag:**`, `${use.tag}`)
-	.addField(`**ID:**`, `${use.id}`)
-	.addField(`**Status:**`, `${mem.presence.status}`)
-	.addField(`**Created at:**`, `${use.createdAt.toUTCString()}`)
-	.addField(`**Acount age:**`, parseTime(Date.now()-use.createdTimestamp))
-	.addField(`**Joined at:**`, `${mem.joinedAt.toUTCString()}`)
-	.addField(`**Highest role:**`, `${mem.highestRole.name}`)
-	.addField(`**display Color?:**`, `${mem.displayHexColor}`)
-	.addField(`**Server Deafened?:**`, `${mem.serverDeaf}`)
-	.addField(`**Server muted?:**`, `${mem.serverMute}`)
-	.addField(`**Roles:**`, `${mem.roles.map(r => r).slice(1,14).join(' | ')}`)
-	.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
-	message.channel.send({embed: embed});
+module.exports = {
+	path: '../../commands/userinfo.js',
+	help: {
+		name: 'userinfo',
+		alias: 'ui',
+		enabled: true,
+		guildOnly: true,
+		permLevel: 0,
+		description: 'Returns a users info in the server',
+		useage: 'userinfo (@user)'
+	},
+	run: async (client,message) => {
+		    let name = message.content.replace( /  +/g, ' ').split(" ").slice(1).join(" ");
+			var mem = message.mentions.members.first()||message.guild.members.get(name)||message.guild.members.find(m => m.displayName.toLowerCase().includes(name.toLowerCase()));
+			if (mem) {
+				var embed = new Discord.RichEmbed()
+				.setColor(`${mem.displayHexColor == '#000000' ? '#00ffff' : mem.displayHexColor}`)
+				.setTimestamp()
+				.setThumbnail(`${mem.user.avatarURL}`)
+				.setAuthor(`Info on: ${mem.user.tag}`, mem.user.displayAvatarURL)
+				.addField(`**ID:**`, `${mem.user.id}`)
+				.addField(`**Status:**`, `${mem.presence.status}`)
+				.addField(`**Created:**`, `${moment(mem.user.createdAt).format('MMMM Do YYYY, h:mm a')}`)
+				.addField(`**Joined:**`, `${moment(mem.joinedAt).format('MMMM Do YYYY, h:mm a')}`)
+				.addField(`**Highest role:**`, `${mem.highestRole.name}`)
+				.addField(`**Hoist role?:**`, `${mem.hoistRole.name}`)
+				.addField(`**display Color:**`, `${mem.displayHexColor}`)
+				.addField(`**Roles:**`, `${mem.roles.map(r => r).slice(1,16).join(' | ')}`)
+				.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
+				message.channel.send(embed);
+			}
+			if (!mem) {
+				var embed = new Discord.RichEmbed()
+				.setColor(`${message.member.displayHexColor == '#000000' ? '#00ffff' : message.member.displayHexColor}`)
+				.setTimestamp()
+				.setThumbnail(`${message.author.avatarURL}`)
+				.setAuthor(`Info on: ${message.author.tag}`, message.author.displayAvatarURL)
+				.addField(`**ID:**`, `${message.author.id}`)
+				.addField(`**Status:**`, `${message.member.presence.status}`)
+				.addField(`**Created:**`, `${moment(message.author.createdAt).format('MMMM Do YYYY, h:mm a')}`)
+				.addField(`**Joined:**`, `${moment(message.member.joinedAt).format('MMMM Do YYYY, h:mm a')}`)
+				.addField(`**Highest role?:**`, `${message.member.highestRole.name}`)
+				.addField(`**Hoist role:**`, `${message.member.hoistRole.name}`)
+				.addField(`**Display Color:**`, `${message.member.displayHexColor}`)
+				.addField(`**Roles:**`, `${message.member.roles.map(r => r).slice(1,16).join(' | ')}`)
+				.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
+				message.channel.send(embed);
+			}
 	}
-	if (!mem) {
-		var embed = new Discord.RichEmbed()
-	.setColor(`${message.member.displayHexColor}`)
-	.setTimestamp()
-	.setThumbnail(`${message.author.avatarURL}`)
-	.setTitle(`Userinfo for: ${message.member.displayName}`)
-	.addField(`**Username & tag:**`, `${message.author.tag}`)
-	.addField(`**ID:**`, `${message.author.id}`)
-	.addField(`**Status:**`, `${message.member.presence.status}`)
-	.addField(`**Created at:**`, `${message.author.createdAt.toUTCString()}`)
-	.addField(`**Acount age:**`, parseTime(Date.now()-message.author.createdTimestamp))
-	.addField(`**Joined at:**`, `${message.member.joinedAt.toUTCString()}`)
-	.addField(`**Highest role:**`, `${message.member.highestRole.name}`)
-	.addField(`**Display Color?:**`, `${message.member.displayHexColor}`)
-	.addField(`**Server Deafened?:**`, `${message.member.serverDeaf}`)
-	.addField(`**Server muted?:**`, `${message.member.serverMute}`)
-	.addField(`**Roles:**`, `${message.member.roles.map(r => r).slice(1,14).join(' | ')}`)
-	.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
-	message.channel.send({embed: embed});
-	console.log(`${message.author.username} got thier userinfo in ${message.guild}`)
-	}
-
-}
-module.exports.help = {
-    name: "userinfo",
-    alias: "ui"
 }
