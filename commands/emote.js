07e023b17ae6f7ec2a111be7eff79ade64bae2e6
kat <34391969+kat0154@users.noreply.github.com>
2019-10-31 00:57:11 -0400

    Y e e t

--- commands/emote.js
+++ commands/emote.js
@@ -1,18 +1,23 @@
-const Discord = require("discord.js");
+let discord = require('discord.js');
 
-module.exports.run = async (client,message,args) => {
-	
-const search = message.content.split(" ").slice(1).join(" ");
-if (!search)return message.channel.send("Please enter an emoji to search for");
-if(search){
-	let emote = client.emojis.find("name", search);
-	if(!emote)return message.channel.send("I couldn't find any emojis with that name");
-	message.channel.send(`${emote}`);
+module.exports.help: {
+		name: 'emoji',
+		alias: 'emote',
+		description: 'searches for an emoji in the clients emoji db',
+		useage: 'emoji <emoji name|emoji ID>'
 }
-	
-	
-}
-module.exports.help = {
-    name: "emote",
-	alias: "emoji"
+module.exports.run = async (client, message) => {
+		let search = message.content.replace( /  +/g, ' ').split(" ").slice(1).join(" ");
+		if (!search)return message.channel.send("Please enter an emoji to search for");
+		if(search){
+			if(search.includes('<'))search = search.split(':')[1].split(':')[0];
+			let em = client.emojis.find(e => e.name == search)||client.emojis.get(search);
+			if(!em)return message.channel.send("I couldn't find any emojis with that name/id");
+			let emote = em.url;
+			let embed = new discord.RichEmbed()
+			.setTitle(em.name)
+			.setColor('#00ffff')
+			.setImage(emote)
+			message.channel.send(embed);
+		}
 }
