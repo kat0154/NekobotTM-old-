68bf168a03648d0be86773edda60355ada55010e
kat <34391969+kat0154@users.noreply.github.com>
2019-10-31 02:27:35 -0400

    Y e e t

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,31 +1,26 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (client,message,args) => {
-
-let ppl = message.mentions.users.first();
-	let oof = message.mentions.members.first();
-	if (ppl) {
-		var RichEmbed = new Discord.RichEmbed()
-     .setColor(`${oof.displayHexColor}`)
-	 .setTimestamp()
-	 .setImage(`${ppl.avatarURL}`)
-	 .setTitle(`Avatar for: ${oof.displayName}`)
-	 .setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
-	 message.channel.send({embed: RichEmbed})
-	}
-	if (!ppl) {
-		var RichEmbed = new Discord.RichEmbed()
-		.setColor(`${message.member.displayHexColor}`)
-		.setTimestamp()
-		.setImage(`${message.author.avatarURL}`)
-		.setTitle(`Avatar for: ${message.member.displayName}`)
-		.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
-		message.channel.send({embed: RichEmbed})
-	}
-	console.log(`avatar command has been used by ${message.author.username} in ${message.guild}`);
-
-}
+const discord = require("discord.js");
 
 module.exports.help = {
-    name: "avatar"
+        name: "avatar",
+        alias: "av",
+        type: "Misc",
+        info: "Gets a users avatar",
+        perms: "MEMBER / none",
+        useage: `@user / {prefix}avatar`
+    }
+module.exports.run = async (client,message) => {
+        let sum1 = message.content.replace( /  +/g, ' ').split(' ').slice(1).join(' ');
+        if(sum1.toLowerCase().includes("avatar"))sum1 = message.content.replace( /  +/g, ' ').split(' ').slice(2).join(' ');
+        let user = message.mentions.users.find(m => m.id !== client.user.id)||client.users.filter(u => u.username.toLowerCase().includes(sum1.toLowerCase())).first()||client.users.get(sum1)||message.author;
+        if(user == message.author && sum1){
+          user = await client.fetchUser(sum1);
+        }
+        let link = user.displayAvatarURL.includes('gif') ? `${user.displayAvatarURL}?size=2048` : user.displayAvatarURL;
+        let embed = new discord.RichEmbed()
+        .setDescription(`**Avatar for:** ${user.username}\n[link](${link})`)
+        .setColor('#00ffff')
+        .setTimestamp()
+	.setImage(`${link}`)
+	.setFooter(`Requested by: ${message.member.displayName}`, `${message.author.displayAvatarURL}`)
+        return message.channel.send(embed);
 }
